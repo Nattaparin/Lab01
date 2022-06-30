ce579e2e785c5783e73b5b746ee591c2d762e707
Nattaparin <nattaparin455@gmail.com>
2022-06-30 22:36:05 +0700

    done

--- main.js
+++ main.js
@@ -1,1 +1,43 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+
+        return {
+           product : 'Shoes',
+           info : 'This is Shoes ',
+           image: './assets/images/socks_green.jpg',
+           link: 'http://www.camt.cmu.ac.th/index.php/th/',
+            inStock : true,
+            inventory: 100,
+            details: ['50% cotton' , '30% wool','20% polyester ' ],
+            variants : [
+                { id: 2234,color:'green', image: './assets/images/socks_green.jpg' },
+                {id: 2235 , color: 'blue', image: './assets/images/socks_blue.jpg'}
+
+            ],
+            size: ['Size = S,M,L'],
+            cart: 0 ,
+            count:0
+
+
+        }
+
+    },
+    methods:{
+        addToCart(){
+            this.cart += 1
+        },
+        updateImage(variantImage){
+            this.image = variantImage
+        },
+        changeStatus() {
+            this.count += 1
+            if(this.count%2 !== 0){
+                this.inventory = 0
+            }
+            else if(this.count%2 === 0) {
+                this.inventory = 100
+            }
+
+        }
+    }
+})
